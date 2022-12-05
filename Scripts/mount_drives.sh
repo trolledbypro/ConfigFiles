3c63b8d0bf92aaa81f1bb9d08e5ad2fdc3a99928
Zachary Zalass <zachzalass@gmail.com>
2022-12-05 11:35:18 -0500

    Added script for drive mounting

---
+++ Scripts/mount_drives.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install the packages
+sudo apt update
+sudo apt install cifs-utils
+sudo apt install sshfs
+
+# Mount SAE drive
+sudo mkdir /mnt/sae
+sudo mount.cifs //10.108.160.36/sae /mnt/sae -o user=vpit
+
+# Mount SAE Archive drive
+sudo mkdir /mnt/saearchive
+sudo mount.cifs //10.108.160.36/saearchive /mnt/saearchive -o user=vpit
+
+# Mount ENCS drive
+sudo mkdir /mnt/unixdrive
+sudo sshfs -o allow_other,default_permissions z_zalass@login.encs.concordia.ca:/home/z/z_zalass /mnt/unixdrive
