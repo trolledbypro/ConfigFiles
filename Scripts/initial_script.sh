c67aa1a5fcc0406539886a10a7a56c2991be113e
Zachary Zalass <zachzalass@gmail.com>
2022-12-05 21:24:46 -0500

    More bugfixes

--- Scripts/initial_script.sh
+++ Scripts/initial_script.sh
@@ -11,12 +11,17 @@ sudo apt install -y snapd
 sudo apt install -y remmina
 sudo snap install thunderbird
 sudo snap install spotify
+sudo apt install -y curl
 
 # Get git
 sudo apt install -y git
 git config --global user.name "Zachary Zalass"
 git config --global user.name "zachzalass@gmail.com"
-git clone https://github.com/trolledbypro/AERO-482-Project.git ~/
+mkdir ~/Source
+git clone https://github.com/trolledbypro/ConfigFiles.git ~/Source/ConfigFiles
+git checkout -b Linux
+git pull
+sudo cp -a ~/Source/ConfigFiles/. ~/
 
 # Install Visual Studio Code and MS Teams
 #sudo apt install -y software-properties-common apt-transport-https
@@ -30,12 +35,15 @@ sudo snap install --classic code
 sudo snap refresh
 
 # Install oh-my-posh
+sudo apt install -y --reinstall python3-apt
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 brew update && brew upgrade oh-my-posh
 exec bash
 
-# Install Python
+# Install Python 3.11
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update 
 sudo apt install -y python3.11-full
